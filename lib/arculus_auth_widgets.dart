d6dd48df0151926c24c38b9471b355a507982d6d
Moses Karunia <moseskarunia@gmail.com>
2021-01-09 16:33:37 +0700

    triggers workflow

--- lib/arculus_auth_widgets.dart
+++ lib/arculus_auth_widgets.dart
@@ -6,5 +6,5 @@ export './src/generic_google_button.dart';
 export './src/arculus_email_button.dart';
 export './src/arculus_apple_button.dart';
 export './src/arculus_google_button.dart';
-// export './src/google_icon.dart';
-// export './src/apple_icon.dart';
+export './src/google_icon.dart';
+export './src/apple_icon.dart';
