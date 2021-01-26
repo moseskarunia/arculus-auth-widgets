a0b321e189182278bc84d1c50d12e035c866d9e7
Moses Karunia <moseskarunia@gmail.com>
2021-01-26 18:09:10 +0700

    delete generic

--- test/src/base_arculus_button_test.dart
+++ test/src/base_arculus_button_test.dart
@@ -82,101 +82,95 @@ void main() {
     expect(icon, findsOneWidget);
     expect(tester.widget(icon).runtimeType, SizedBox);
   });
-  group('button label', () {
-    testWidgets(
-      'should be visible when !isLoading',
-      (tester) async {
-        await tester.pumpWidget(
-          MaterialApp(
-            home: Scaffold(
-              body: BaseArculusButton(
-                label: 'Test Label',
-                icon: SizedBox(key: Key('icon')),
-              ),
+
+  testWidgets(
+    'button label should be visible',
+    (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: BaseArculusButton(
+              label: 'Test Label',
+              icon: SizedBox(key: Key('icon')),
             ),
           ),
-        );
+        ),
+      );
 
-        final text = find.descendant(
-          of: find.byKey(Key('arculus-base-button-main-child')),
-          matching: find.byKey(Key('arculus-base-button-label')),
-        );
+      final text = find.descendant(
+        of: find.byKey(Key('arculus-base-button-main-child')),
+        matching: find.byKey(Key('arculus-base-button-label')),
+      );
 
-        expect(text, findsOneWidget);
+      expect(text, findsOneWidget);
 
-        final padding = find.ancestor(
-          of: text,
-          matching: find.byKey(Key('arculus-base-button-label-padding')),
-        );
-        expect(padding, findsOneWidget);
+      final padding = find.ancestor(
+        of: text,
+        matching: find.byKey(Key('arculus-base-button-label-padding')),
+      );
+      expect(padding, findsOneWidget);
 
-        expect(
-          tester.widget<Padding>(padding).padding,
-          const EdgeInsets.only(left: 16),
-        );
-        expect(
-          find.byKey(Key('arculus-base-button-progress-indicator')),
-          findsNothing,
-        );
-      },
-    );
-
-    testWidgets(
-      'should be invisible when isLoading and '
-      'show loading indicator instead',
-      (tester) async {
-        await tester.pumpWidget(
-          MaterialApp(
-            home: Scaffold(
-              body: BaseArculusButton(
-                isLoading: true,
-                label: 'Test Label',
-                icon: SizedBox(key: Key('icon')),
-              ),
+      expect(
+        tester.widget<Padding>(padding).padding,
+        const EdgeInsets.only(left: 16),
+      );
+      expect(
+        find.byKey(Key('arculus-base-button-progress-indicator')),
+        findsNothing,
+      );
+    },
+  );
+  testWidgets(
+    'loading indicator should be visible when isLoading',
+    (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: BaseArculusButton(
+              isLoading: true,
+              label: 'Test Label',
+              icon: SizedBox(key: Key('icon')),
             ),
           ),
-        );
+        ),
+      );
 
-        final text = find.descendant(
-          of: find.byKey(Key('arculus-base-button-main-child')),
-          matching: find.byKey(Key('arculus-base-button-label')),
-        );
+      final progressIndicator = find.descendant(
+        of: find.byKey(Key('arculus-base-button-main-child')),
+        matching: find.byKey(Key('arculus-base-button-progress-indicator')),
+      );
 
-        expect(text, findsNothing);
+      expect(progressIndicator, findsOneWidget);
 
-        final progressIndicator = find.descendant(
-          of: find.byKey(Key('arculus-base-button-main-child')),
-          matching: find.byKey(Key('arculus-base-button-progress-indicator')),
-        );
+      final outerSizedBox = find.ancestor(
+        of: progressIndicator,
+        matching:
+            find.byKey(Key('arculus-base-button-progress-indicator-sized-box')),
+      );
 
-        expect(progressIndicator, findsOneWidget);
+      expect(outerSizedBox, findsOneWidget);
+      expect(tester.widget<SizedBox>(outerSizedBox).width, 12);
+      expect(tester.widget<SizedBox>(outerSizedBox).height, 12);
+    },
+  );
 
-        final outerSizedBox = find.ancestor(
-          of: progressIndicator,
-          matching: find.byKey(Key('arculus-base-button-icon-sized-box')),
-        );
-
-        expect(outerSizedBox, findsOneWidget);
-        expect(tester.widget<SizedBox>(outerSizedBox).width, 18);
-        expect(tester.widget<SizedBox>(outerSizedBox).height, 18);
-
-        final outerCenter = find.ancestor(
-          of: outerSizedBox,
-          matching: find.byKey(Key('arculus-base-button-icon-center')),
-        );
-
-        expect(outerCenter, findsOneWidget);
-        expect(tester.widget(outerCenter).runtimeType, Center);
-
-        final outerExpanded = find.ancestor(
-          of: outerCenter,
-          matching: find.byKey(Key('arculus-base-button-icon-expanded')),
-        );
-
-        expect(outerExpanded, findsOneWidget);
-        expect(tester.widget(outerExpanded).runtimeType, Expanded);
-      },
+  testWidgets('should find Spacer if isExpanded', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: BaseArculusButton(
+            label: 'Test Label',
+            icon: SizedBox(key: Key('icon')),
+            isExpanded: true,
+          ),
+        ),
+      ),
     );
+
+    final spacer = find.byKey(Key('arculus-base-button-spacer'));
+
+    expect(spacer, findsOneWidget);
+    expect(tester.widget(spacer).runtimeType, Spacer);
   });
 
   group('foregroundColor', () {
